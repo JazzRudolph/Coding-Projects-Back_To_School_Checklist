2953da3a40e2f967cd2c82a3b39e858ece33543e
Jazz Rudolph <jazzrudolph92@outlook.com>
2025-07-03 01:18:56 -0400

    Sort items puting the completed on the bottom. When I come back I want to add a delete button.

--- script.js
+++ script.js
@@ -33,31 +33,40 @@ function updateItem(item, key, value) {
 }
 
 function refreshList() {
-    // Clear the current list
-
-    ITEM_CONTAINER.innerHTML = "";
-
-    for (const item of items) {
-        const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
-        const descriptionInput = itemElement.querySelector(".item-description");
-        const completedInput = itemElement.querySelector(".item-completed");
-
-        descriptionInput.value = item.description;
-        completedInput.checked = item.completed;
-
-        descriptionInput.addEventListener('change', () => {
-            updateItem(item, "description", descriptionInput.value);
-        });
-
-        completedInput.addEventListener('change', () => {
-            updateItem(item, "completed", completedInput.checked);
-        });
-
-
-        ITEM_CONTAINER.appendChild(itemElement);
-
-    }
+    items.sort((a, b) => {
+        if (a.completed) {
+            return 1;
+        }
+        if (b.completed) {
+            return -1;
+        }
+        return a.description < b.description ? -1 : 1;
+    });
+    
 }
+
+ITEM_CONTAINER.innerHTML = "";
+
+for (const item of items) {
+    const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+    const descriptionInput = itemElement.querySelector(".item-description");
+    const completedInput = itemElement.querySelector(".item-completed");
+
+    descriptionInput.value = item.description;
+    completedInput.checked = item.completed;
+
+    descriptionInput.addEventListener('change', () => {
+        updateItem(item, "description", descriptionInput.value);
+        });
+
+    completedInput.addEventListener('change', () => {
+        updateItem(item, "completed", completedInput.checked);
+        });
+
+
+    ITEM_CONTAINER.appendChild(itemElement);
+
+}  
 
 ADD_BUTTON.addEventListener('click', () => {
     addItem();
